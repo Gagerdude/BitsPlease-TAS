b7bfec025a6b7e3fae6c37c59a532852b1944702
Zachary Atkins <zacharyjayhawk@gmail.com>
2020-02-08 05:13:01 -0600

    added skeleton

--- tas/tas.ino
+++ tas/tas.ino
@@ -4,11 +4,40 @@
 #define DELAY_4US asm volatile ("nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n")
 
 
+void readCommand();
+void sendByte(byte);
+
 void setup() {
   
 }
 
 
 void loop() {
+  
+}
+
+void readCommand() {
+  command = 0x00; // replace with read
+
+  switch (command) {
+    case 0x00: // STATUS
+      sendByte(0x05);
+      sendByte(0x00);
+      // sendByte(bitFlags);
+      break;
+    case 0x02: // POLL
+      // send button and stick state
+      break;
+    case 0x03: // READ
+      break;
+    case 0x04: // WRITE
+      break;
+    default:
+      // some kind of error handling?
+      break;
+  }
+}
+
+void sendCommand(byte b) {
   
 }
